5907361935289bb13cb5e94aba7be369686a37f9
Khoirul Anwar <shedenk@gmail.com>
2025-12-19 21:54:55 +0700

    new

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ minio_client = Minio(
     MINIO_ENDPOINT,
     access_key=MINIO_ACCESS_KEY,
     secret_key=MINIO_SECRET_KEY,
-    secure=False
+    secure=MINIO_PUBLIC_BASE_URL.startswith("https")
 )
 
 if not minio_client.bucket_exists(MINIO_BUCKET):
